7264a4835b61fd75328137cbf6a6e7e3d420bba1
Walter Mattingly <waltdakind@gmail.com>
2016-11-16 15:33:59 +0000

    basic functionality, no styling

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,70 @@
 
 // get lat and lon 
+var url,
+    apiurl,
+    lat,
+    lon,
+    desc,
+    temp,
+    tempC,
+    hum,
+    city,
+    code;
 
 function getLocation() {
     if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
+        navigator.geolocation.getCurrentPosition(logPosition);
     } else {
         alert("Geolocation is not supported by this browser.");
     }
 }
-function showPosition(position) {
+function logPosition(position) {
 lat = position.coords.latitude;
 lon = position.coords.longitude; 
-console.log(lat", "+ lon);
+console.log(lat + ", "+ lon);
+makeUrl(lat, lon);
 }
+
+// create the url for the api by lat and lon
+// api.openweathermap.org/data/2.5/weather?lat=35&lon=139
+function makeUrl(lat, lon){
+apiurl= 'http://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&APPID=fe96a3e1611ec2b7c051b6b642af8eb9';
+console.log(apiurl);
+callApi(apiurl);
+}
+
 
 
 // api call by lat and lon
 
 
+function callApi(apiurl){
+    $.ajax({url: apiurl, success: function(result){
+       // $(".container").html(result);
+        console.log(result);
+        //url for png
+        url = "http://openweathermap.org/img/w/" + result.weather[0].icon + ".png"
+        city = result.name;
+        desc = result.weather[0].description;
+        temp = result.main.temp * 9/5 - 459.67;
+        tempC = result.main.temp  - 273.15;
+        hum = result.main.humidity;
+        console.log(url);
+        $("#url").attr("src", url);
+        console.log(city);
+        $("#city").text(city);
+        console.log(desc);
+        $("#desc").text(desc);
+        console.log(temp + " F");
+        $("#temp").text(temp);
+        console.log(tempC + " C"); 
+        $("#humidity").text(hum + "%");      
+        console.log(hum + "%");
+    }});
+}
 
-//url for png
+$(function(){
+getLocation();
 
-var url = "http://openweathermap.org/img/w/" + code + ".png"
 
-
+});
