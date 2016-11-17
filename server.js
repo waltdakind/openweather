4ec39cc9c69b5db38a5b993d81a5518218d8d897
Walter Mattingly <waltdakind@gmail.com>
2016-11-17 01:48:25 +0000

    more https debugging

---
+++ server.js
@@ -0,0 +1,11 @@
+var http = require('http');
+var https = require('https');
+var express = require('express');
+var app = express();
+
+//Lets define a port we want to listen to
+const PORT= process.env.PORT || 8080; 
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(process.env.PORT || 8080);
